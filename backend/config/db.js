f3170e82f17c7bc43a20d2b12dc3aeeeb6f9a577
MandhojTheeng <theengmandhoj@gmail.com>
2025-04-02 13:34:42 +0545

    Successfully connected MongoDB

---
+++ backend/config/db.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+export const connectDB = async()=>{
+    try{
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    }catch(error){
+        console.error(`Error: ${error.message}`);
+
+        // process code where 1 means exit with failure, 0 means success
+        process.exit(1);
+
+    }
+}
